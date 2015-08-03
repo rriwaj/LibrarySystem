8fd51c64e587c7122e8c45a0ad8922a0edfb4b1c
3ouri <abdallah.ouri@hotmail.com>
2015-08-02 21:41:12 -0500

    added new package

---
+++ LibrarySystem/src/ls/controller/common/CommonConfigurations.java
@@ -0,0 +1,9 @@
+package ls.controller.common;
+
+public class CommonConfigurations {
+
+	public interface LabelMessages {
+		public final static String OPERATION_DONE_SUCCESSFULLY = "Operation Done Successfully";
+		public final static String OPERATION_FAILED = "Operation Failed";
+	}
+}
